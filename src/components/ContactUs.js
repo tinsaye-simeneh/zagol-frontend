61551244df0b3570925aba3dabde71bcaf05a185
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-03-22 22:11:09 +0300

    contact us fixed

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -9,39 +9,21 @@ import '../index.css';
 
 const ContactUs = () => {
   return (
-    <div className="container-fluid bg-black">
+    <div className="container-fluid bg-silver py-4">
       <div className="row text-center">
         <div className="col-md-6">
           <h1
-            className="caros text-white px-5 pt-5 leftText"
+            className="caros text-dark px-5 pt-5 leftText"
             style={{fontWeight: "bold" }}
           >
             Lets Chat <br />
-            Tell me about <br /> your project
+            Tell me about <br /> your Concerns
           </h1>
           <p
-            className="caros text-white px-5 pt-2"
+            className="caros text-dark px-5 pt-2"
           >
             Lets create something better
           </p>
-          <div className="row mt-5 mb-4">
-            <div className="col-12">
-            <img
-              src={Mail}
-              alt="email"
-              className="px-2"
-              style={{ width: "12%" }}
-            />
-            <img src={linkedin} alt="linkedin" 
-              className="px-2" style={{ width: "12%" }} />
-            <img src={twitter} alt="twitter" 
-              className="px-2" style={{ width: "12%" }} />
-            <img src={medium} alt="medium" 
-              className="px-2" style={{ width: "12%" }} />
-            <img src={dev} alt="dev" 
-              className="px-2" style={{ width: "12%" }} />
-            </div>
-          </div>
         </div>
         <div className="col col-sm-12 col-md-6 text-center pb-5">
           <form className="p-5 rounded bg-white text-center">
