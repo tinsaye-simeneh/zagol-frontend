a0419391410acbd7341dd7c354eb0b4600fa9a0c
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2023-03-23 21:12:23 +0300

    d

--- src/components/Home.js
+++ src/components/Home.js
@@ -94,7 +94,7 @@ const Home = () => {
           data-slide="prev"
         >
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only text-dark">Previous</span>
+          <span class="sr-only">Previous</span>
         </a>
         <a
           class="carousel-control-next bg-dark"
