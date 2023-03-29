4b5ce7d61217776f48d1f3df0f7ad3d4b92321f7
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2023-03-29 23:33:16 +0300

    feat: Carousel buttons UI update

--- src/components/Home.js
+++ src/components/Home.js
@@ -88,19 +88,25 @@ const Home = () => {
           </div>
         </div>
         <a
-          class="carousel-control-prev bg-dark"
+          class="carousel-control-prev bg-dark carousel-btn"
           href="#carouselExampleControls"
           role="button"
           data-slide="prev"
+          style={{
+            width: "7%",
+          }}
         >
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
         <a
-          class="carousel-control-next bg-dark"
+          class="carousel-control-next bg-dark carousel-btn"
           href="#carouselExampleControls"
           role="button"
           data-slide="next"
+          style={{
+            width: "7%",
+          }}
         >
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
